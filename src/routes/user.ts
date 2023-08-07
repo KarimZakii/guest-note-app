b789a2e001009230cbfd4b6c61aec29eda2f1cdc
KarimZakii <116194338+KarimZakii@users.noreply.github.com>
2023-08-08 02:44:28 +0300

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,6 @@ import {
   sendNote,
   latestNotes,
   deleteNote,
-  test,
 } from "../controller/user.controller";
 import { auth } from "../middleware/auth";
 const userRouter = Router();
@@ -11,6 +10,5 @@ const userRouter = Router();
 userRouter.post("/sendnote", sendNote);
 userRouter.get("/getnotes", auth, latestNotes);
 userRouter.post("deleteNote", auth, deleteNote);
-userRouter.get("/test", test);
 
 export default userRouter;
